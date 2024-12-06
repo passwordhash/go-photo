956ed3242af2f7df55466e65b3a3a107635f559c
Molodtsov Yaroslav <105973491+passwordhash@users.noreply.github.com>
2024-12-06 15:38:24 +0300

    схема изменена: уход от повсеместного использования uuid к serial

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,13 +1,13 @@
 CREATE TABLE Folders (
-  id UUID PRIMARY KEY,
-  folder_path TEXT
+  id SERIAL PRIMARY KEY,
+  folder_path TEXT not null
 );
 
 CREATE TABLE Photos (
-  id UUID PRIMARY KEY,
-  user_id UUID,
-  filename VARCHAR(255),
-  folder_id UUID,
+  id SERIAL PRIMARY KEY,
+  user_uuid UUID not null ,
+  filename VARCHAR(255) not null ,
+  folder_id INTEGER,
   uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (folder_id) REFERENCES Folders (id)
 );
@@ -15,11 +15,15 @@ CREATE TABLE Photos (
 CREATE TYPE version_type_enum AS ENUM ('original', 'thumbnail', 'preview');
 
 CREATE TABLE PhotoVersions (
-  id UUID PRIMARY KEY,
-  photo_id UUID,
-  version_type version_type_enum,
-  filepath VARCHAR(255),
-  width INTEGER,
-  height INTEGER,
+  id SERIAL PRIMARY KEY,
+  photo_id INTEGER not null ,
+  version_type version_type_enum default 'original',
+  filepath VARCHAR(255) not null ,
+  width INTEGER not null,
+  height INTEGER not null,
+  size INTEGER not null,
   FOREIGN KEY (photo_id) REFERENCES Photos (id)
 );
+
+CREATE INDEX idx_photos_user_uuid ON Photos (user_uuid);
+CREATE INDEX idx_photo_versions_photo_id ON PhotoVersions (photo_id);
